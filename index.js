a87d22faa91f5e8f0cb186882e63012923cb5b4e
Henrique de Sousa <153359075+hrsousadam@users.noreply.github.com>
2023-12-10 11:34:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World / FUNCIONA CARAI')
 })
 
 app.listen(process.env.PORT || 3000)
